02e1a6a2a25bebcf2d0e6d4600f689b7926a6af2
jh871 <jennyh871@gmail.com>
2023-02-03 12:10:52 +0000

    working ajax, displays results, some styling for new div

--- script.js
+++ script.js
@@ -7,27 +7,55 @@ let searchTerm = "";
 
 submitBtn.on(`click`, function (event){
     event.preventDefault();
-
     let searchTerm = searchValue.val();
     console.log(searchTerm);
+    //retrieving data via ajax first
 
+//changing url from test to article search API proper
+let queryURL = 
+    ("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=ARLR7Y6y9Zqrp8NKHTGpWCr4gIOj6L9U");
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response); //this works
+        console.log(response.response);
+        console.log(response.response.docs);
+        //testing path to headline:
+        console.log(response.response.docs[0].headline.main);
+        //path to abstract:
+        console.log(response.response.docs[0].abstract);
+        
+        //generating elements to hold selected content
     //creating place for article
+
+    
+    //div for single article, within Articles div
     let article = $("<div>");
-    article.attr("class", "articleResult")
-    article.text(searchTerm);
+    article.attr("class", "articleResult");
     articlesDiv.append(article);
+
+    //article title in single article div 
+    let articleTitle = $("<h2>");
+    articleTitle.attr("id","article-title");
+    articleTitle.text(response.response.docs[0].headline.main);
+    article.append(articleTitle);
+
+    //text for single article in single article div
+    let articleText = $("<p>");
+    articleText.attr("id", "article-text");
+    articleText.text(response.response.docs[0].abstract)
+    article.append(articleText);
+    })
+
+
+//need for loop within button click that adds more results depending on dropdown number
+
+//need to clear results between clicks for when topic changes
 
 });
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=arts&api-key=ARLR7Y6y9Zqrp8NKHTGpWCr4gIOj6L9U";
 
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(response) {
-        console.log(response); //this works
-    })
 
 
 
