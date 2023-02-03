0c3a47f1bbb4ee06b4b4282b223cb666fa7a549e
jh871 <jennyh871@gmail.com>
2023-02-03 11:39:07 +0000

    added place for article, plus basic styling

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const searchValue = $(`#search`)
 const submitBtn = $(`#submitBtn`)
 
+//grabbing articles div
+let articlesDiv = $(".articles");
+let searchTerm = "";
 
 submitBtn.on(`click`, function (event){
     event.preventDefault();
 
     let searchTerm = searchValue.val();
-
     console.log(searchTerm);
 
-});
+    //creating place for article
+    let article = $("<div>");
+    article.attr("class", "articleResult")
+    article.text(searchTerm);
+    articlesDiv.append(article);
 
+});
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=arts&api-key=ARLR7Y6y9Zqrp8NKHTGpWCr4gIOj6L9U";
 
@@ -19,7 +26,7 @@ $.ajax({
     url: queryURL,
     method: "GET"
     }).then(function(response) {
-        console.log(response);
+        console.log(response); //this works
     })
 
 
