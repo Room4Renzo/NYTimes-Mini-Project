1998957c716fb4338ebd3301fa1a1acda9d83a4c
Renzo Francis-Walker <Lorenzo.afw@gmail.com>
2023-02-03 07:19:35 +0000

    Made some changes to the css. Items now display in the correct order and one underneath the last. Also added classes for easierreferencing of elements and surrounded form item titles in p tags

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const searchValue = $(`#search`)
-const submitBtn = $(`#submitBtn`)
+const searchValue = $(`#search`);
+const submitBtn = $(`#submitBtn`);
+const dropBtn = $(`#dropBtn`);
+const startYear = $(`#startYear`);
+const endYear = $(`#endYear`);
+const clearResults = $(`clearResults`);
 
-
-submitBtn.on(`click`, function (event){
+submitBtn.on(`click`, function (event) {
     event.preventDefault();
 
     let searchTerm = searchValue.val();
@@ -11,17 +14,15 @@ submitBtn.on(`click`, function (event){
 
 });
 
-
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=arts&api-key=ARLR7Y6y9Zqrp8NKHTGpWCr4gIOj6L9U";
+let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=arts&api-key=ARLR7Y6y9Zqrp8NKHTGpWCr4gIOj6L9U";
 
 
 $.ajax({
     url: queryURL,
     method: "GET"
-    }).then(function(response) {
-        console.log(response);
+}).then(function (response) {
+    console.log(response);
     })
-
 
 
 
