1551caa37448feb9aed5edcffa868fe7bfc24328
Renzo Francis-Walker <Lorenzo.afw@gmail.com>
2023-02-04 00:51:01 +0000

    Tried to have a calendar pop up when search bar is clicked to make choosing a start and end date easier. Will read through NYT API and find out how to capture the input and amend the query based on that information once i get the calendar to function correctly

--- script.js
+++ script.js
@@ -30,45 +30,56 @@ submitBtn.on(`click`, function (event) {
         method: "GET"
     }).then(function (response) {
 
-        let dropBtn = $(`.dropBtn`).val();
+        $(function () {
+            $(startYear).datepicker({
+                showOn: `click`,
+                // buttonImage: "./Images\Calendar 50.png",
+            })
+        })
+    })
 
-        queryURL = ("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=ARLR7Y6y9Zqrp8NKHTGpWCr4gIOj6L9U" + limit + dropBtn);
+    let dropBtn = $(`.dropBtn`).val();
+    queryURL = ("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=ARLR7Y6y9Zqrp8NKHTGpWCr4gIOj6L9U" + limit + dropBtn);
+
+    //Testing search fields with console logs
+    console.log(response)
+    console.log(response.response.docs[0].pub_date)
 
 
-        //Empties previous results
-        let article
-        $(clearResults).on("click", function () {
-            $(articlesDiv).empty();
-        });
 
-        // Loop to create the divs for requested number of article results
-        for (i = 0; i < dropBtn; i++) {
-
-
-            //div for single article, within Articles div
-            article = $("<div>");
-            article.attr("class", "articleResult");
-            articlesDiv.prepend(article);
-
-            //article title in single article div 
-            let articleTitle = $("<h2>");
-            articleTitle.attr("id", "article-title");
-            articleTitle.text(response.response.docs[i].headline.main);
-            article.prepend(articleTitle);
-
-            //text for single article in single article div
-            let articleText = $("<p>");
-            articleText.attr("id", "article-text");
-            articleText.text(response.response.docs[i].abstract)
-            article.append(articleText);
-        }
+    //Empties previous results
+    let article
+    $(clearResults).on("click", function () {
+        $(articlesDiv).empty();
     });
-})
+
+    // Loop to create the divs for requested number of article results
+    for (i = 0; i < dropBtn; i++) {
+
+
+        //div for single article, within Articles div
+        article = $("<div>");
+        article.attr("class", "articleResult");
+        articlesDiv.prepend(article);
+
+        //article title in single article div 
+        let articleTitle = $("<h2>");
+        articleTitle.attr("id", "article-title");
+        articleTitle.text(response.response.docs[i].headline.main);
+        article.prepend(articleTitle);
+
+        //text for single article in single article div
+        let articleText = $("<p>");
+        articleText.attr("id", "article-text");
+        articleText.text(response.response.docs[i].abstract)
+        article.append(articleText);
+    }
+});
 
 // if option [i].value = 1, 2, 3, 4, 5 set querurl to limit =
 // console.log(option[0].value); //returns "1"
 // console.log(option[1].value); //returns "2"
-
+// Calendar image credit: <a target="_blank" href="https://icons8.com/icon/37877/tear-off-calendar">Tear-Off Calendar</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 
 
 
